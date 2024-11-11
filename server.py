b3162409994354ea77433e459c8bbf53f52a5324
gri11 <kanawatgrill2002@gmail.com>
2024-11-11 20:37:34 +0700

    fix

--- server.py
+++ server.py
@@ -14,7 +14,6 @@ load_dotenv()
 
 class ComparisonService(compare_pb2_grpc.ComparisonServiceServicer):
     def __init__(self):
-<<<<<<< HEAD
         # Initialize MongoDB client
         self.db = ComparisonDatabase(os.getenv("MONGODB_URI"), os.getenv("MONGODB_DB"), os.getenv("MONGODB_COLLECTION"))
 
@@ -28,24 +27,6 @@ class ComparisonService(compare_pb2_grpc.ComparisonServiceServicer):
         result = self.db.get_comparison(event_id, date)
         
         # print(result)
-=======
-        # Initialize MongoDB client with database and collection
-        self.mongo_client = MongoClient(os.getenv("MONGO_URI"))
-        # self.db = self.mongo_client[os.getenv("MONGO_DB")]
-        # self.collection = self.db[os.getenv("MONGO_COLLECTION")]
-
-    def GetComparison(self, request, context):
-        # Attempt to parse news_id as an integer
-        try:
-            news_id = int(request.news_id)
-        except ValueError:
-            context.set_code(grpc.StatusCode.INVALID_ARGUMENT)
-            context.set_details("news_id must be an integer")
-            return compare_pb2.Comparison()
-
-        # Query MongoDB for the specified news_id
-        result = self.collection.find_one({"news_id": news_id})
->>>>>>> origin/main
 
         if result:
             # Create a Comparison message to hold the response data
@@ -53,13 +34,8 @@ class ComparisonService(compare_pb2_grpc.ComparisonServiceServicer):
 
             # Map similarities and differences to KeyValue pairs
             entries = {
-<<<<<<< HEAD
                 "similarities": result['comparison']['similarities'],
                 "differences": result['comparison']['differences'],
-=======
-                "similarities": result.get("similarities", []),
-                "differences": result.get("differences", []),
->>>>>>> origin/main
             }
 
             for key, values in entries.items():
@@ -80,14 +56,9 @@ def serve():
     compare_pb2_grpc.add_ComparisonServiceServicer_to_server(
         ComparisonService(), server
     )
-<<<<<<< HEAD
     port = os.getenv("PORT", "50053")
     server.add_insecure_port(f"[::]:{port}")
     print(f"Server starting on port {port}...")
-=======
-    server.add_insecure_port("[::]:50054")
-    print("Server starting on port 50054...")
->>>>>>> origin/main
     server.start()
     server.wait_for_termination()
 
